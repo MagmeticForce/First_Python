8dcc4e93f8429ac5523edaf2ab0e4ac8fb89357c
MagmeticForce <qaz7788.br@gmail.com>
2025-08-05 10:52:51 -0400

    Update First_Python.py
    Fixed syntax errors.

--- First_Python.py
+++ First_Python.py
@@ -2,15 +2,13 @@ int, int_2 = 3, 7;
 sum = int + int_2;
 
 
-print ('Hello. Sum is: ')
-print (sum)
+print ('Hello. Sum is: ');
+print (sum);
 
 
 
-def swap(array_input)
-{
+def swap(array_input):
+
     """Swap two indices of an array."""
-    
-    if (array_input.length <= 0)
-    {print('Could not perform wap. Array is empty!');}
-}
+    if (array_input.length <= 0):
+        print('Could not perform wap. Array is empty!');
