13a830e84afc755ce45be10625627966da456a64
MagmeticForce <qaz7788.br@gmail.com>
2025-08-05 10:49:56 -0400

    Create First_Python.py

---
+++ First_Python.py
@@ -0,0 +1,16 @@
+int, int_2 = 3, 7;
+sum = int + int_2;
+
+
+print ('Hello. Sum is: ')
+print (sum)
+
+
+
+def swap(array_input)
+{
+    """Swap two indices of an array."""
+    
+    if (array_input.length <= 0)
+    {print('Could not perform wap. Array is empty!');}
+}
