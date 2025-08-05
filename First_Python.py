723a6e568e9178f50db691c87310b135c3e06640
MagmeticForce <qaz7788.br@gmail.com>
2025-08-05 10:58:47 -0400

    Update First_Python.py

--- First_Python.py
+++ First_Python.py
@@ -6,9 +6,17 @@ print ('Hello. Sum is: ');
 print (sum);
 
 
+my_array_of_numbers = [0, 1, 2, 3, 5, 4];
 
-def swap(array_input):
+
+def swap(array_input, first_index_to_swap_input, second_index_to_swap_input):
 
     """Swap two indices of an array."""
     if (array_input.length <= 0):
-        print('Could not perform wap. Array is empty!');
+        print('Could not perform swap. Array is empty!');
+        
+    temp = array_input[second_index_to_swap_input];
+    array_input[second_index_to_swap_input] = array_input[first_index_to_swap_input];
+    array_input[first_index_to_swap_input] = temp;
+    
+    
